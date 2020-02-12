c8189928f4fc6bb6741aa9afd35a9e73707f820f
Jared M Schooley <jaschoo@siue.edu>
2020-02-12 12:48:07 -0600

    Reading files in as strings done, need to probably convert these to proper yaml structs then json though, this will be the next step.

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"mime/multipart"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
@@ -14,11 +16,35 @@ type BindFile struct {
 	File  *multipart.FileHeader `form:"file" binding:"required"`
 }
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
 	router := gin.Default()
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
 	router.Static("/", "./public")
+
+	// Get current working directory
+	myDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(myDir)
+	// Return list of all yaml file locations
+	yamlFiles, err := WalkMatch(fmt.Sprintf("%s/public/yaml/", myDir), "*.yaml")
+	fmt.Println(yamlFiles)
+	fmt.Println(err)
+
+	// Read all yaml files into an array
+	var strArray []string
+	for i, v := range yamlFiles {
+		readFile(v, &strArray)
+		fmt.Println(strArray[i])
+	}
 	router.POST("/upload", func(c *gin.Context) {
 		var bindFile BindFile
 
@@ -39,4 +65,35 @@ func main() {
 		c.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, bindFile.Name, bindFile.Email))
 	})
 	router.Run(":8080")
+}
+
+func WalkMatch(root, pattern string) ([]string, error) {
+	var matches []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if matched, err := filepath.Match(pattern, filepath.Base(path)); err != nil {
+			return err
+		} else if matched {
+			matches = append(matches, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return matches, nil
+}
+
+func readFile(filePath string, yaml *[]string) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	*yaml = append(*yaml, string(data))
 }
